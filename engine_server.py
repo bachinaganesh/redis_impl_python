4719411ef5cb35ab495a091fe8efb7bbfa3de90b
Bachina Ganesh <567ganeshb@gmail.com>
2025-11-05 13:42:16 +0530

    Added web socket io connection also to send the more task updates

--- engine_server.py
+++ engine_server.py
@@ -34,23 +34,37 @@ def process_task(task_id, payload):
     logger.info(f"Fetching lookup data and task id: {task_id}")
     time.sleep(5)
     # when task1 is completed here update the prgress to 15 percentage
-    r.hset(f"task:{task_id}", mapping={"status": "processing", "progress": "15"})
+    publish_progress(task_id, "processing", "20")
 
     # Task2: Processing strategy 
     logger.info(f"Processing strategy resuly and task id: {task_id}")
     time.sleep(10)
     # when task2 is completed here update the progress to 50 percentage
-    r.hset(f"task:{task_id}", mapping={"status": "processing", "progress": "50"})
+    publish_progress(task_id, "processing", "75")
 
     # Task3: Store the data to database
     logger.info(f"Store the data into database and task id: {task_id}")
     time.sleep(5)
     # when task3 is completed update the staus and progress is 100 
-    r.hset(f"task:{task_id}", mapping={"status": "completed", "progress": "100"})
+    publish_progress(task_id, "completed", "100", "Task is completed")
     # send the json response 
 
 
-    print(f"Payload completed for task : {task_id}")
+def publish_progress(task_id, status, progress, result = None):
+    # send the progress back to UI based in socket connection
+    task_update = {
+        "task_id": task_id,
+        "status": status,
+        "progress": progress
+    }
+
+    if result:
+        task_update["result"] = result
+
+    r.hset(f"task:{task_id}", mapping=task_update)
+    r.publish(f"task_update:{task_id}", json.dumps(task_update))
+
+
 
 def start_engine():
     pool = Pool(processes=5)
--- qb_manager.py
+++ qb_manager.py
@@ -3,9 +3,10 @@ from fastapi.responses import JSONResponse
 from redis import asyncio as aioredis
 import uuid
 import json
+import asyncio
 
 app = FastAPI()
-r = aioredis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_async = aioredis.Redis(host="localhost", port=6379, decode_responses=True)
 
 async def enqueue_task(payload: dict):
     task_id = str(uuid.uuid4())
@@ -14,8 +15,8 @@ async def enqueue_task(payload: dict):
         "payload": payload,
         "status": "queued"
     }
-    await r.lpush("task_queue", json.dumps(task))
-    await r.hset(f"task:{task_id}", mapping={"status": "queued", "progress": "0"})
+    await redis_async.lpush("task_queue", json.dumps(task))
+    await redis_async.hset(f"task:{task_id}", mapping={"status": "queued", "progress": "0"})
     return task_id
 
 @app.post("/submit-task")
@@ -31,7 +32,7 @@ async def submit_task(request: Request):
 @app.get("/status/{task_id}")
 async def task_status(task_id):
     task_key = f"task:{task_id}"
-    task_data = await r.hgetall(task_key)
+    task_data = await redis_async.hgetall(task_key)
     if not task_data:
         return JSONResponse({
             "error": "Task not found with an id: "+task_id
@@ -44,3 +45,23 @@ async def task_status(task_id):
             "progress": task_data.get("progress", "0")
         },
         status_code=200)
+    
+
+@app.websocket("/ws/{task_id}")
+async def websocket_endpoint(websocket: WebSocket, task_id: str):
+    await websocket.accept()
+    try:
+        pub = redis_async.pubsub()
+        await pub.subscribe(f"task_update:{task_id}")
+        # if connection fails then get the update from redis_hash instaed of redis pub/sub
+        task_data = await redis_async.hgetall(f"task:{task_id}")
+        task_data["task_id"] = task_id
+        if task_data:
+            await websocket.send_text(json.dumps(task_data))
+        while True:
+            message = await pub.get_message(ignore_subscribe_messages=True, timeout=1.0)
+            if message:
+                await websocket.send_text(message["data"])
+            await asyncio.sleep(0.1)
+    except WebSocketDisconnect:
+        print(f"WebSocket disconnected for task {task_id}")
